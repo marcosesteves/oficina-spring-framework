849de9037ec6d364ea3648ef305b4d309f5fb35a
Marco Aurelio <profmarcosesteves@gmail.com>
2017-12-11 19:07:00 -0200

    commit 1

---
+++ oficina-spring-framework/src/main/java/com/algaworks/vinhos/OficinaSpringFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.vinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaSpringFrameworkApplication.class, args);
+	}
+}
